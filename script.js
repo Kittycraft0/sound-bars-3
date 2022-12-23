bb916114547f2b11ec1e25827f0d601751ce1dcc
Kitty Craft0 <107146715+Kittycraft0@users.noreply.github.com>
2022-12-23 02:29:22 +0000

    it works right now i guess

--- script.js
+++ script.js
@@ -172,12 +172,12 @@ function playSound(pitch,type,length,volume,low,high){
   
   var loop1=setInterval(()=>{
     if(Math.random()<=1/4){
-      playSound(0,"sine",Math.floor(Math.random()*2000/12)*12+500,0.01,200,800);
+      playSound(0,"sine",Math.floor(Math.random()*2000/12)*12+500,0.05,200,800);
       n0++;
       //console.log("0: "+n0);
     }
     if(Math.random()<=1/8){
-      playSound(0,"sine",Math.floor(Math.random()*4000/12)*12+500,0.1,0,200);
+      playSound(0,"sine",Math.floor(Math.random()*4000/12)*12+500,0.1,1,200);
       n1++;
       //console.log("1: "+n1);
     }
